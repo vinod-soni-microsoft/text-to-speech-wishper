c5ccda3acd97e938bb803a95eeb1270bc013eabf
Vinod Soni <117770765+vinod-soni-microsoft@users.noreply.github.com>
2024-01-04 18:46:42 +0000

    condition to validate sound file

--- src/main.py
+++ src/main.py
@@ -5,19 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-with open(Path.cwd()/"src"/"lost_debit_card.wav","rb") as audio_file:
+with open(Path.cwd()/"src"/"graduation-day.wav","rb") as audio_file:
   transcript = openai.Audio.transcribe("whisper-1",audio_file)
-  #print(transcript)
+  print(transcript)
 
-  response = openai.ChatCompletion.create(
-      model="gpt-4",
-      messages=[
-          {"role": "system", "content": "You are a sentiment classification bot, print out if the user is happy or sad"},
-          {"role": "user", "content": transcript.text}
-      ],
-      temperature=0.7,
-      max_tokens=150,
-  )
+  if transcript.text == "" or transcript.text == "":
+      print("The audio file was not found or is not valid, so no speech sound was detected.")
+  else:
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are a sentiment classification bot, print out if the user is happy or sad"},
+            {"role": "user", "content": transcript.text}
+        ],
+        temperature=0.7,
+        max_tokens=150,
+    )
 
-  response_message = response["choices"][0]["message"]
-  print(response_message)
+    response_message = response["choices"][0]["message"]
+    print(response_message)
